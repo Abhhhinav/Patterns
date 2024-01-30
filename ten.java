10c28eda8a2bd58b2a601cd046be3af86de5c4b1
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2024-01-31 02:48:17 +0530

    Add files via upload

---
+++ ten.java
@@ -0,0 +1,16 @@
+package pattern;
+
+public class ten {
+    public static void main(String[] args) {
+    for(int k =1;k<=5;k++){
+       for(int i =1;i<=(5-k);i++){
+         System.out.print(" ");
+       }
+
+       for(int i=1;i<=5;i++){
+         System.out.print("*");
+       }
+        System.out.println();
+       }
+    }
+}
